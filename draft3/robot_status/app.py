95858afbbefcc683369e07637d523bd65b403902
mhmdk <mhmdkassas@live.com>
2020-09-06 11:49:58 -0700

    home page added

--- draft3/robot_status/app.py
+++ draft3/robot_status/app.py
@@ -34,86 +34,81 @@ class Draft3:
         def index():
             return render_template('index.html')
 
-        @app.route("/GPIO1/", methods=['GET', 'POST'])
+        @app.route("/gpio1/", methods=['GET', 'POST'])
         def gpio1_finder():
             self.gpio1 = random.randint(1, 50)
-            return render_template('index.html', GPIO1=self.gpio1, GPIO2=self.gpio2, ultrasonic=self.ultrasonic, IR=self.infrared, encoders=self.encoders, jointx1 = self.jointx1, jointx2=self.jointx2, jointx3 = self.jointx3, US1 = self.us1, US2 = self.us2, US3 = self.us3, US4 = self.us4, left=self.left, center=self.center, right=self.right)
+            return render_template('index.html', gpio1=self.gpio1, gpio2=self.gpio2, ultrasonic=self.ultrasonic, infrared=self.infrared, encoders=self.encoders, jointx1 = self.jointx1, jointx2=self.jointx2, jointx3 = self.jointx3, us1 = self.us1, us2 = self.us2, us3 = self.us3, us4 = self.us4, left=self.left, center=self.center, right=self.right)
 
-        @app.route("/GPIO2/", methods=['GET', 'POST'])
+        @app.route("/gpio2/", methods=['GET', 'POST'])
         def gpio2_finder():
             self.gpio2 = random.randint(50, 100)
-            return render_template('index.html', GPIO1=self.gpio1, GPIO2=self.gpio2, ultrasonic=self.ultrasonic, IR=self.infrared, encoders=self.encoders, jointx1 = self.jointx1, jointx2=self.jointx2, jointx3 = self.jointx3, US1 = self.us1, US2 = self.us2, US3 = self.us3, US4 = self.us4, left=self.left, center=self.center, right=self.right)
-
-
+            return render_template('index.html', gpio1=self.gpio1, gpio2=self.gpio2, ultrasonic=self.ultrasonic, infrared=self.infrared, encoders=self.encoders, jointx1 = self.jointx1, jointx2=self.jointx2, jointx3 = self.jointx3, us1 = self.us1, us2 = self.us2, us3 = self.us3, us4 = self.us4, left=self.left, center=self.center, right=self.right)
+  
         @app.route("/ultrasonic/", methods=['GET', 'POST'])
         def ultrasonic_finder():
             self.ultrasonic= random.randint(100, 150)
-            return render_template('index.html', GPIO1=self.gpio1, GPIO2=self.gpio2, ultrasonic=self.ultrasonic, IR=self.infrared, encoders=self.encoders, jointx1 = self.jointx1, jointx2=self.jointx2, jointx3 = self.jointx3, US1 = self.us1, US2 = self.us2, US3 = self.us3, US4 = self.us4, left=self.left, center=self.center, right=self.right)
-   
+            return render_template('index.html', gpio1=self.gpio1, gpio2=self.gpio2, ultrasonic=self.ultrasonic, infrared=self.infrared, encoders=self.encoders, jointx1 = self.jointx1, jointx2=self.jointx2, jointx3 = self.jointx3, us1 = self.us1, us2 = self.us2, us3 = self.us3, us4 = self.us4, left=self.left, center=self.center, right=self.right)
 
         @app.route("/infrared/", methods=['GET', 'POST'])
         def infrared_finder():
             self.infrared= random.randint(100, 150)
-            return render_template('index.html', GPIO1=self.gpio1, GPIO2=self.gpio2, ultrasonic=self.ultrasonic, IR=self.infrared, encoders=self.encoders, jointx1 = self.jointx1, jointx2=self.jointx2, jointx3 = self.jointx3, US1 = self.us1, US2 = self.us2, US3 = self.us3, US4 = self.us4, left=self.left, center=self.center, right=self.right) 
+            return render_template('index.html', gpio1=self.gpio1, gpio2=self.gpio2, ultrasonic=self.ultrasonic, infrared=self.infrared, encoders=self.encoders, jointx1 = self.jointx1, jointx2=self.jointx2, jointx3 = self.jointx3, us1 = self.us1, us2 = self.us2, us3 = self.us3, us4 = self.us4, left=self.left, center=self.center, right=self.right)
 
         @app.route("/encoders/", methods=['GET', 'POST'])
         def encoders_finder():
             self.encoders = random.randint(100, 150)
-            return render_template('index.html', GPIO1=self.gpio1, GPIO2=self.gpio2, ultrasonic=self.ultrasonic, IR=self.infrared, encoders=self.encoders, jointx1 = self.jointx1, jointx2=self.jointx2, jointx3 = self.jointx3, US1 = self.us1, US2 = self.us2, US3 = self.us3, US4 = self.us4, left=self.left, center=self.center, right=self.right)
+            return render_template('index.html', gpio1=self.gpio1, gpio2=self.gpio2, ultrasonic=self.ultrasonic, infrared=self.infrared, encoders=self.encoders, jointx1 = self.jointx1, jointx2=self.jointx2, jointx3 = self.jointx3, us1 = self.us1, us2 = self.us2, us3 = self.us3, us4 = self.us4, left=self.left, center=self.center, right=self.right)
 
-
-        @app.route("/jointx1/", methods=['GET', 'POST'])
+        @app.route("/jointx/", methods=['GET', 'POST'])
         def jointx1_finder():
             self.jointx1= random.randint(100, 150)
-            return render_template('index.html', GPIO1=self.gpio1, GPIO2=self.gpio2, ultrasonic=self.ultrasonic, IR=self.infrared, encoders=self.encoders, jointx1 = self.jointx1, jointx2=self.jointx2, jointx3 = self.jointx3, US1 = self.us1, US2 = self.us2, US3 = self.us3, US4 = self.us4, left=self.left, center=self.center, right=self.right)
+            return render_template('index.html', gpio1=self.gpio1, gpio2=self.gpio2, ultrasonic=self.ultrasonic, infrared=self.infrared, encoders=self.encoders, jointx1 = self.jointx1, jointx2=self.jointx2, jointx3 = self.jointx3, us1 = self.us1, us2 = self.us2, us3 = self.us3, us4 = self.us4, left=self.left, center=self.center, right=self.right)
 
-        
-        @app.route("/jointx2/", methods=['GET', 'POST'])
+        @app.route("/jointy/", methods=['GET', 'POST'])
         def jointx2_finder():
             self.jointx2= random.randint(100, 150)
-            return render_template('index.html', GPIO1=self.gpio1, GPIO2=self.gpio2, ultrasonic=self.ultrasonic, IR=self.infrared, encoders=self.encoders, jointx1 = self.jointx1, jointx2=self.jointx2, jointx3 = self.jointx3, US1 = self.us1, US2 = self.us2, US3 = self.us3, US4 = self.us4, left=self.left, center=self.center, right=self.right)
+            return render_template('index.html', gpio1=self.gpio1, gpio2=self.gpio2, ultrasonic=self.ultrasonic, infrared=self.infrared, encoders=self.encoders, jointx1 = self.jointx1, jointx2=self.jointx2, jointx3 = self.jointx3, us1 = self.us1, us2 = self.us2, us3 = self.us3, us4 = self.us4, left=self.left, center=self.center, right=self.right)
 
-
-        @app.route("/jointx3/", methods=['GET', 'POST'])
+        @app.route("/jointz/", methods=['GET', 'POST'])
         def jointx3_finder():
             self.jointx3= random.randint(100, 150)
-            return render_template('index.html', GPIO1=self.gpio1, GPIO2=self.gpio2, ultrasonic=self.ultrasonic, IR=self.infrared, encoders=self.encoders, jointx1 = self.jointx1, jointx2=self.jointx2, jointx3 = self.jointx3, US1 = self.us1, US2 = self.us2, US3 = self.us3, US4 = self.us4, left=self.left, center=self.center, right=self.right)
+            return render_template('index.html', gpio1=self.gpio1, gpio2=self.gpio2, ultrasonic=self.ultrasonic, infrared=self.infrared, encoders=self.encoders, jointx1 = self.jointx1, jointx2=self.jointx2, jointx3 = self.jointx3, us1 = self.us1, us2 = self.us2, us3 = self.us3, us4 = self.us4, left=self.left, center=self.center, right=self.right)
 
         @app.route("/us1/", methods=['GET', 'POST'])
         def us1_finder():
             self.us1= random.randint(100, 150)
-            return render_template('index.html', GPIO1=self.gpio1, GPIO2=self.gpio2, ultrasonic=self.ultrasonic, IR=self.infrared, encoders=self.encoders, jointx1 = self.jointx1, jointx2=self.jointx2, jointx3 = self.jointx3, US1 = self.us1, US2 = self.us2, US3 = self.us3, US4 = self.us4, left=self.left, center=self.center, right=self.right)
+            return render_template('index.html', gpio1=self.gpio1, gpio2=self.gpio2, ultrasonic=self.ultrasonic, infrared=self.infrared, encoders=self.encoders, jointx1 = self.jointx1, jointx2=self.jointx2, jointx3 = self.jointx3, us1 = self.us1, us2 = self.us2, us3 = self.us3, us4 = self.us4, left=self.left, center=self.center, right=self.right)
 
         @app.route("/us2/", methods=['GET', 'POST'])
         def us2_finder():
             self.us2= random.randint(100, 150)
-            return render_template('index.html', GPIO1=self.gpio1, GPIO2=self.gpio2, ultrasonic=self.ultrasonic, IR=self.infrared, encoders=self.encoders, jointx1 = self.jointx1, jointx2=self.jointx2, jointx3 = self.jointx3, US1 = self.us1, US2 = self.us2, US3 = self.us3, US4 = self.us4, left=self.left, center=self.center, right=self.right)
-
+            return render_template('index.html', gpio1=self.gpio1, gpio2=self.gpio2, ultrasonic=self.ultrasonic, infrared=self.infrared, encoders=self.encoders, jointx1 = self.jointx1, jointx2=self.jointx2, jointx3 = self.jointx3, us1 = self.us1, us2 = self.us2, us3 = self.us3, us4 = self.us4, left=self.left, center=self.center, right=self.right)
+        
         @app.route("/us3/", methods=['GET', 'POST'])
         def us3_finder():
             self.us3= random.randint(100, 150)
-            return render_template('index.html', GPIO1=self.gpio1, GPIO2=self.gpio2, ultrasonic=self.ultrasonic, IR=self.infrared, encoders=self.encoders, jointx1 = self.jointx1, jointx2=self.jointx2, jointx3 = self.jointx3, US1 = self.us1, US2 = self.us2, US3 = self.us3, US4 = self.us4, left=self.left, center=self.center, right=self.right)
+            return render_template('index.html', gpio1=self.gpio1, gpio2=self.gpio2, ultrasonic=self.ultrasonic, infrared=self.infrared, encoders=self.encoders, jointx1 = self.jointx1, jointx2=self.jointx2, jointx3 = self.jointx3, us1 = self.us1, us2 = self.us2, us3 = self.us3, us4 = self.us4, left=self.left, center=self.center, right=self.right)
 
         @app.route("/us4/", methods=['GET', 'POST'])
         def us4_finder():
             self.us4= random.randint(100, 150)
-            return render_template('index.html', GPIO1=self.gpio1, GPIO2=self.gpio2, ultrasonic=self.ultrasonic, IR=self.infrared, encoders=self.encoders, jointx1 = self.jointx1, jointx2=self.jointx2, jointx3 = self.jointx3, US1 = self.us1, US2 = self.us2, US3 = self.us3, US4 = self.us4, left=self.left, center=self.center, right=self.right)
+            return render_template('index.html', gpio1=self.gpio1, gpio2=self.gpio2, ultrasonic=self.ultrasonic, infrared=self.infrared, encoders=self.encoders, jointx1 = self.jointx1, jointx2=self.jointx2, jointx3 = self.jointx3, us1 = self.us1, us2 = self.us2, us3 = self.us3, us4 = self.us4, left=self.left, center=self.center, right=self.right)
 
         @app.route("/left/", methods=['GET', 'POST'])
         def left_finder():
             self.left= random.randint(100, 150)
-            return render_template('index.html', GPIO1=self.gpio1, GPIO2=self.gpio2, ultrasonic=self.ultrasonic, IR=self.infrared, encoders=self.encoders, jointx1 = self.jointx1, jointx2=self.jointx2, jointx3 = self.jointx3, US1 = self.us1, US2 = self.us2, US3 = self.us3, US4 = self.us4, left=self.left, center=self.center, right=self.right)
+            return render_template('index.html', gpio1=self.gpio1, gpio2=self.gpio2, ultrasonic=self.ultrasonic, infrared=self.infrared, encoders=self.encoders, jointx1 = self.jointx1, jointx2=self.jointx2, jointx3 = self.jointx3, us1 = self.us1, us2 = self.us2, us3 = self.us3, us4 = self.us4, left=self.left, center=self.center, right=self.right)
 
         @app.route("/center/", methods=['GET', 'POST'])
         def center_finder():
             self.center= random.randint(100, 150)
-            return render_template('index.html', GPIO1=self.gpio1, GPIO2=self.gpio2, ultrasonic=self.ultrasonic, IR=self.infrared, encoders=self.encoders, jointx1 = self.jointx1, jointx2=self.jointx2, jointx3 = self.jointx3, US1 = self.us1, US2 = self.us2, US3 = self.us3, US4 = self.us4, left=self.left, center=self.center, right=self.right)
+            return render_template('index.html', gpio1=self.gpio1, gpio2=self.gpio2, ultrasonic=self.ultrasonic, infrared=self.infrared, encoders=self.encoders, jointx1 = self.jointx1, jointx2=self.jointx2, jointx3 = self.jointx3, us1 = self.us1, us2 = self.us2, us3 = self.us3, us4 = self.us4, left=self.left, center=self.center, right=self.right)
 
         @app.route("/right/", methods=['GET', 'POST'])
         def right_finder():
             self.right= random.randint(100, 150)
-            return render_template('index.html', GPIO1=self.gpio1, GPIO2=self.gpio2, ultrasonic=self.ultrasonic, IR=self.infrared, encoders=self.encoders, jointx1 = self.jointx1, jointx2=self.jointx2, jointx3 = self.jointx3, US1 = self.us1, US2 = self.us2, US3 = self.us3, US4 = self.us4, left=self.left, center=self.center, right=self.right)
-
+            return render_template('index.html', gpio1=self.gpio1, gpio2=self.gpio2, ultrasonic=self.ultrasonic, infrared=self.infrared, encoders=self.encoders, jointx1 = self.jointx1, jointx2=self.jointx2, jointx3 = self.jointx3, us1 = self.us1, us2 = self.us2, us3 = self.us3, us4 = self.us4, left=self.left, center=self.center, right=self.right)
+            
         app.run()
 
 if __name__ == '__main__':
